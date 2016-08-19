d172ff1f521023bbb18c95359c79d4af7541f9ad
Albert <kovalevskij.albert@gmail.com>
2016-08-19 12:24:34 +0300

    babelrc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import redis from 'redis';
-import Promise from 'bluebird';
-Promise.promisifyAll(redis);
+import bluebird from 'bluebird';
+bluebird.promisifyAll(redis);
 
 export default function (config) {
   return redis.createClient(config);
