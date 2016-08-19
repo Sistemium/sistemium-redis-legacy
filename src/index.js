b702da1d566af741448f1d9d50bb75566b6e47e4
Albert <kovalevskij.albert@gmail.com>
2016-08-19 12:13:04 +0300

    engines

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import redis from 'redis';
 import Promise from 'bluebird';
 Promise.promisifyAll(redis);
 
-export default function () {
-  return redis.createClient();
+export default function (config) {
+  return redis.createClient(config);
 }
